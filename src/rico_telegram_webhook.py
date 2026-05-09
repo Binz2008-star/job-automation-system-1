372968179247ae6c96081ac6145c93083c42c299
Roben edwan <loyal_ro@hotmail.com>
2026-05-09 05:50:36 +0400

    telegram: route callback queries through Rico UI handlers

--- src/rico_telegram_webhook.py
+++ src/rico_telegram_webhook.py
@@ -5,11 +5,15 @@ from __future__ import annotations
 from typing import Any, Dict
 
 from src.rico_chat_api import RicoChatAPI
+from src.rico_telegram_ui import handle_callback_only
 
 chat_api = RicoChatAPI()
 
 
 def process_telegram_update(update: Dict[str, Any]) -> Dict[str, Any]:
+    if update.get("callback_query"):
+        return handle_callback_only(update)
+
     message = update.get("message", {})
     chat = message.get("chat", {})
     user = message.get("from", {})
